1067960a3d38253769d8ced984eac5d7636a55c0
cecilio alvarez caules <cecilioalvarezcaules@iMac-de-cecilio.local>
2022-01-07 13:50:36 +0100

    final

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -19,6 +19,31 @@ app.post("/facturas",function(req,res) {
     res.status(201).send();
 
 })
+app.put("/facturas/:numero",function(req,res) {
+
+
+  let seleccionado=null;
+  for (let i=0;i<facturas.length;i++) {
+
+    if (facturas[i].numero==req.params.numero) {
+
+        seleccionado=facturas[i];
+        break;
+    }
+  
+  }
+  console.log(seleccionado);
+  console.log(req.body);
+  if(seleccionado!=null){
+    seleccionado.numero=req.body.numero;
+    seleccionado.concepto=req.body.concepto
+    seleccionado.importe =req.body.importe;
+    console.log(facturas);
+  }
+  res.status(200).send();
+
+})
+
 //añademe al api rest un metodo de delete 
 app.delete("/facturas/:numero",function(req,res) {
 
